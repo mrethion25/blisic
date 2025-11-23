f94c52ad8b75cd0e1e26cd4c92666d17cd26cc47
mrethion25 <worldof2512@gmail.com>
2025-11-23 16:52:19 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,11 +17,11 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Hayasaka",
+  title: "blisic",
   description: "Music streaming app",
   image:
     "https://res.cloudinary.com/dbr73rpz9/image/upload/v1690380865/images/logo-color_noktgr.png",
-  url: "https://hayasaka.vercel.app",
+  url: "https://blisic.vercel.app/",
   type: "website",
   icons: [{ rel: "icon", url: Favicon.src }],
   site_name: "Hayasaka",
